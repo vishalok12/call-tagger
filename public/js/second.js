9a96d4ae10a3f3acce04a44678f57bd0b1b32d0b
Vishal Kumar <vishal.rgiit@gmail.com>
2016-09-03 04:11:16 +0530

    some state

--- public/js/second.js
+++ public/js/second.js
@@ -1,37 +1,72 @@
-$(document).ready(function(){
-    // for dropdown
-    $(".selectbox").click(function() {
-        $(".dd-wrap").addClass('active');
+(function() {
+    $(document).ready(function(){
+        $.get('/sounds').then(function(sounds) {
+            fillDropdown(sounds);
+        });
+
+        // for dropdown
+        $(".selectbox").click(function() {
+            $(".dd-wrap").addClass('active');
+        });
+
+        // $('html').click(function() {
+        //     $(".dd-wrap").removeClass('active');
+        // });
+
+        $('.dd-wrap').click(function(event){
+            event.stopPropagation();
+        });
+
+
+        var wavesurfer = WaveSurfer.create({
+            container: '#waveform',
+            waveColor: 'violet',
+            progressColor: 'purple'
+        });
+        
+        function genwave(file) {
+            $(".dd-wrap").removeClass('active');
+
+            wavesurfer.load('/' + file);
+
+            let postData =  {
+               "fileName": '/input/' + file
+            };
+
+            $.post('/fileTag', postData).then(function(output) {
+                console.log(output);
+            }, function(e) {
+                console.log(e);
+            })
+        }
+
+        wavesurfer.on('ready', function () {
+            wavesurfer.play();
+        });
+
+        $('#waveform').click(function(){
+            wavesurfer.playPause();
+        });
+
+        $('#sound-list').on('click', '.file', function(e) {
+            let sound = $(e.target).data('value');
+
+            genwave(sound);
+        });
     });
 
-    $('html').click(function() {
-        $(".dd-wrap").removeClass('active');
-    });
+    function fillDropdown(soundList) {
+        let $lists = $();
 
-    $('.dd-wrap').click(function(event){
-        event.stopPropagation();
-    });
+        soundList.map(function(sound, index) {
+            let $li = $('<li class="file clearfix" id="file-' + index + '" data-value="' + sound + '">');
+            $li.append($('<span class="file-name">').text(sound));
+            $li.append($('<span class="process" title="start processing">')
+                .append($('<i class="fa fa-play">')));
 
+            $lists = $lists.add($li);
+        });
 
-    var wavesurfer = WaveSurfer.create({
-        container: '#waveform',
-        waveColor: 'violet',
-        progressColor: 'purple'
-    });
-    
-    function genwave() {
-        wavesurfer.load('audio/test.mp3');
+        $('#sound-list').append($lists);
     }
-
-    wavesurfer.on('ready', function () {
-        wavesurfer.play();
-    });
-
-    $('#waveform').click(function(){
-        wavesurfer.playPause();
-    });
-
-    document.getElementById('file1').addEventListener('click', function() {
-        genwave()
-    });
-})
+})();
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@
 const express = require('express'),
 	router = express.Router(),
 	getTagForAudio = require('../index'),
-	cachedResponses = require('../cachedResponses');
+	cachedResponses = require('../cachedResponses'),
+	dir = require('node-dir'),
+	path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,6 +16,21 @@ router.get('/', function(req, res, next) {
 router.get('/second', function(req, res, next) {
   res.render('second', { title: 'Call Tagger - PROPTIGER HACKATHON 2016 - THE GAME OF HACKS' });
 });
+
+let inputFiles;
+router.get('/sounds', function(req, res) {
+	if (inputFiles) {
+		return res.send(inputFiles);
+	}
+	dir.files(path.join(__dirname, '..', 'input'), function(error, files) {
+		if (error) {
+			return res.status(500).send({message: error.message});
+		}
+		inputFiles = files.map(file => file.slice(file.lastIndexOf('/') + 1));
+
+		return res.send(inputFiles);
+	});
+})
 
 router.post('/fileTag', function (req, res) {
 	console.log(req.body, "req.body");
